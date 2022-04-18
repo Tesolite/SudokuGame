9c16680addfd57d8fdbb2e40f3939453f959de22
Tesolite <pavel.kotka@gmail.com>
2022-04-18 22:55:20 +0100

    added validBoard method

--- Sudoku.java
+++ Sudoku.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Random;
 
 public class Sudoku {
 
@@ -7,9 +8,13 @@ public class Sudoku {
         //  System.out.println(Arrays.toString(sudoku[2]));
         sudoku[2][5] = 1;
         printGrid(sudoku);
+
         System.out.println(validRows(sudoku));
         System.out.println(validColumns(sudoku));
         System.out.println(validBlocks(sudoku));
+        System.out.println(validBoard(sudoku));
+
+        printGrid(generateBoard(sudoku));
     }
 
 
@@ -18,7 +23,7 @@ public class Sudoku {
         int[][] sudokuGrid = new int[size][size];
         for (int row = 0; row < 9; row++) {
             for (int column = 0; column < 9; column++) {
-                sudokuGrid[row][column] = column;
+                sudokuGrid[row][column] = 0;
             }
         }
 
@@ -53,15 +58,20 @@ public class Sudoku {
 
     private static boolean validColumns(int[][] grid) {
         int[] allValuesInColumn = new int[grid.length];
+        int valueCounter = 0;
         int flag = 0;
 
         for (int column = 0; column < grid.length; column++) {
             for (int rowIndex = 0; rowIndex < grid.length; rowIndex++) {
                 allValuesInColumn[rowIndex] = grid[rowIndex][column];
+                if(grid[rowIndex][column] != 0){
+                    valueCounter++;
+                }
             }
+            System.out.println(Arrays.toString(allValuesInColumn) + ": " + "Values:" + valueCounter + " Distinct:" + Arrays.stream(allValuesInColumn).distinct().count() + "," + "Total:" + allValuesInColumn.length);
 
             if (Arrays.stream(allValuesInColumn).distinct().count() != allValuesInColumn.length) {
-                System.out.println(Arrays.toString(allValuesInColumn));
+                //System.out.println(Arrays.toString(allValuesInColumn));
                 flag = 1;
             }
         }
@@ -126,13 +136,40 @@ public class Sudoku {
 
 
         }
-        if(flag == 1)
-        {
+        if(flag == 1) {
             return false;
         }
         else{
             return true;
         }
+    }
+
+    private static boolean validBoard(int[][] grid){
+        if(validRows(grid) == true &&
+           validColumns(grid) == true &&
+           validBlocks(grid) == true){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    private static int[][] generateBoard(int[][] grid){
+        Random random = new Random();
+
+        grid[0][0] = random.nextInt(9) + 1;
+        grid[1][4] = random.nextInt(9) + 1;
+        grid[2][8] = random.nextInt(9) + 1;
+        grid[3][1] = random.nextInt(9) + 1;
+        grid[4][5] = random.nextInt(9) + 1;
+        grid[5][6] = random.nextInt(9) + 1;
+        grid[6][2] = random.nextInt(9) + 1;
+        grid[7][3] = random.nextInt(9) + 1;
+        grid[8][7] = random.nextInt(9) + 1;
+
+        return grid;
+
     }
 }
 
